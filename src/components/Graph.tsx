758f57ec473b9329e245b2b574468d9f4e3d62ae
Pedro Nascimento <lobo@estudar.com.vc>
2018-09-17 00:34:35 -0300

    Feat: Support for multiple lineplots

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -3,30 +3,27 @@ import Plot from 'react-plotly.js';
 
 export interface PlotProps {
   airspeed: number[];
-  sinkRate: number[];
+  data: number[][];
 }
 
-const plot: React.StatelessComponent<PlotProps> = (props) => {
-  const { airspeed, sinkRate } = props;
-  return (
-    <Plot
-      data={[
-        {
-          x: airspeed,
-          y: sinkRate,
-          type: 'scatter',
-          mode: 'lines+markers',
-          marker: { color: 'red' },
-        },
-      ]}
+const plot: React.StatelessComponent<PlotProps> = ({ airspeed, data }) => {
+  return <Plot
+      data={data.map((d, idx) => (
+      {
+        x: airspeed,
+        y: d,
+        type: 'scatter',
+        mode: 'lines+markers',
+        name: (idx + 1).toString(),
+      } as Partial<Plotly.Data>
+      ))}
       layout={{ autosize: true, title: 'A Fancy Plot' }}
       style={{ width: '100%', height: '100%' }}
       useResizeHandler
       config={{
         modeBarButtonsToRemove: ['sendDataToCloud', 'lasso2d', 'hoverCompareCartesian', 'toggleSpikelines', 'zoom2d'],
       }}
-    />
-  );
+    />;
 };
 
 export default plot;
--- src/views/PerformanceView.tsx
+++ src/views/PerformanceView.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { Container, Col, Row, Nav, NavItem, NavLink } from 'reactstrap';
+import { Container, Col, Row, Nav, NavItem, NavLink, TabContent, TabPane, Button } from 'reactstrap';
 import { Inputs, Plot } from '../components';
 import AtmosIsa from '../util/atmosphere';
 
 export interface PerformanceViewProps {}
 export interface PerformanceViewState {
   airspeed: number[];
-  config: any;
+  config: any[];
+  activeTab: number;
 }
 
 export interface AircraftConfiguration {
@@ -22,46 +23,108 @@ export default class PerformanceView extends React.Component<PerformanceViewProp
 
     this.state = {
       airspeed: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
-      config: {
+      config: [{
         dragCD0: 0, // 0.01805,
         dragK: 0, // 0.05627,
         S: 0, // 153,
         W: 0, // 588600,
-      },
+      }],
+      activeTab: 0,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.toggleActiveTab = this.toggleActiveTab.bind(this);
+    this.addNewTab = this.addNewTab.bind(this);
   }
 
-  handleChange (event: React.FormEvent<HTMLInputElement>) {
-    const { config } = this.state;
-    config[event.currentTarget.name] = event.currentTarget.value;
+  handleChange (idx: number) {
+    return (event: React.FormEvent<HTMLInputElement>) => {
+      const { config } = this.state;
+      config[idx][event.currentTarget.name] = event.currentTarget.value;
 
-    this.setState({ config });
+      this.setState({ config });
+    };
+  }
+
+  addNewTab() {
+    const { config } = this.state;
+    config.push({
+      dragCD0: 0,
+      dragK: 0,
+      S: 0,
+      W: 0,
+    });
+    
+    this.setState(prevState => ({ config, activeTab: prevState.activeTab + 1 }));
   }
 
   calculateSinkRate(h: number) {
-    const { dragK, dragCD0, S, W } = this.state.config;
     const rho = AtmosIsa.density(h);
+    
+    return this.state.config.map((config) => {
+      const { dragK, dragCD0, S, W } = config;  
+    
+      return this.state.airspeed.map(v =>
+        ((rho * S * dragCD0 * v ** 3) / (2 * W) + (2 * dragK * W) / (rho * v * S)));
+    });
+  }
 
-    return this.state.airspeed.map(v => ((rho * S * dragCD0 * v ** 3) / (2 * W) + (2 * dragK * W) / (rho * v * S)));
+  renderNavTabs() {
+    const { activeTab } = this.state;
+
+    return this.state.config.map((_, idx) => {
+      return (
+        <NavItem key={idx}>
+          <NavLink active={idx === activeTab} onClick={this.toggleActiveTab(idx)}>
+            {idx + 1}
+          </NavLink>
+        </NavItem>
+      );
+    });
+  }
+
+  toggleActiveTab(idx: number) {
+    return () => {
+      this.setState({ activeTab: idx });
+    };
+  }
+
+  renderInputTabs() {
+    return this.state.config.map((config, idx) => {
+      return (
+        <TabPane tabId={idx} key={idx}>
+          <Inputs
+            index={idx}
+            changeHandler={this.handleChange(idx)}
+            config={config}
+          />
+        </TabPane>
+      );
+    });
   }
 
   render() {
+    const { airspeed, activeTab } = this.state;
+
     return (
       <main role="main">
         <Container>
           <Row>
             <Col xs="3">
-              <Inputs 
-                changeHandler={this.handleChange}
-                config={this.state.config}
-              />
+              <Nav tabs id="nav-tab" role="tablist">
+                {this.renderNavTabs()}
+              </Nav>
+              <TabContent activeTab={activeTab}>
+                {this.renderInputTabs()}
+              </TabContent>
+            <Button color="primary" onClick={this.addNewTab}>
+              +
+            </Button>
             </Col>
             <Col xs="9">
               <Plot
-                airspeed={this.state.airspeed}
-                sinkRate={this.calculateSinkRate(0)}
+                airspeed={airspeed}
+                data={this.calculateSinkRate(0)}
               />
             </Col>
           </Row>
